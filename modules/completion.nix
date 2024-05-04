c6d08cc00a53084e4d61563405a37b0d0eed36a9
Ryan Halliwell <git@rayliwell.com>
2024-05-04 22:49:26 +0100

    feat: add `completion` module

---
+++ modules/completion.nix
@@ -0,0 +1,27 @@
+{
+  plugins = {
+    cmp = {
+      enable = true;
+      settings = {
+        mapping = {
+          "<C-n>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-p>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
+        sources = [
+          { name = "path"; }
+          { name = "nvim_lsp"; }
+        ];
+      };
+    };
+
+    "cmp-nvim-lsp" = {
+      enable = true;
+    };
+
+    "cmp-path" = {
+      enable = true;
+    };
+  };
+}
--- modules/default.nix
+++ modules/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./git.nix
     ./todo.nix
+    ./completion.nix
     ./treesitter.nix
     ./which-key.nix
     ./telescope.nix

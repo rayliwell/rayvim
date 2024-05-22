1eef31edeabbdd5c21ec10a646f506efa0dfbe8e
Ryan Halliwell <git@rayliwell.com>
2024-05-22 15:37:32 +0100

    chore: update to neovim 0.10

--- flake.nix
+++ flake.nix
@@ -5,16 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    neogit = {
-      url = "github:NeogitOrg/neogit/nightly";
-      flake = false;
-    };
-
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -28,20 +18,13 @@
       flake-utils,
       nixvim,
       ...
-    }@inputs:
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = import ./overlays.nix inputs;
         module = import ./.;
 
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = with overlays; [
-            neovim-nightly
-            neogit-nightly
-          ];
-        };
+        pkgs = import nixpkgs { inherit system; };
       in
       {
         packages = {
--- overlays.nix
+++
@@ -1,9 +0,0 @@
-inputs: {
-  neovim-nightly = inputs.neovim-nightly-overlay.overlay;
-
-  neogit-nightly = final: prev: {
-    vimPlugins = prev.vimPlugins.extend (
-      final': prev': { neogit = prev'.neogit.overrideAttrs { src = inputs.neogit; }; }
-    );
-  };
-}

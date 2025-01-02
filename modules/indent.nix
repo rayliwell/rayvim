cc8cfc565e4e76aea5fe43c67d18e5706212a519
Ryan Halliwell <git@rayliwell.com>
2025-01-02 08:28:04 +0000

    feat: add indent guides from `snacks.nvim`

--- modules/indent.nix
+++ modules/indent.nix
@@ -1,7 +1,39 @@
+{ helpers, ... }:
 {
   plugins = {
     guess-indent = {
       enable = true;
+    };
+
+    snacks = {
+      enable = true;
+
+      settings = {
+        indent = {
+          enable = true;
+        };
+
+        animate = {
+          duration = {
+            step = 20;
+          };
+        };
+      };
+    };
+  };
+
+  colorschemes = {
+    catppuccin = {
+      settings = {
+        custom_highlights = helpers.mkRaw ''
+          function(colors)
+            return {
+              SnacksIndent = { fg = colors.surface0 },
+              SnacksIndentScope = { fg = colors.surface2 },
+            }
+          end
+        '';
+      };
     };
   };
 }
